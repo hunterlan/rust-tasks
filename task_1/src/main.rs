ef52cf3640d025824c47e0f79a913da6ab1c2fcb
hunterlan <konstantin.sharykin@outlook.com>
2024-12-20 08:54:11 +0100

    Task 1 refactoring

--- task_1/src/main.rs
+++ task_1/src/main.rs
@@ -3,8 +3,8 @@ fn main() {
     let a = 2;
     let a4 = (a * a) * (a * a);
     let a6 = (a * a) * (a * a) * (a * a);
-    let a8 = a4 * a4;
-    let a10 = a4 * a6;
+    let _a8 = a4 * a4;
+    let _a10 = a4 * a6;
 
     //Task 2
 
@@ -20,39 +20,39 @@ fn main() {
     let y_result = y_2 - y_1;
     let z_result = z_2 - z_1;
 
-    let square_distance = (x_result * x_result) + (y_result * y_result) + (z_result * z_result);
+    let _square_distance = (x_result * x_result) + (y_result * y_result) + (z_result * z_result);
 
     //Task 3
-    let mut number = 1234;
-    let number_four = number % 10;
-    number /= 10;
-    let number_three = number % 10;
-    number /= 10;
-    let number_two = number % 10;
-    number /= 10;
-    let number_one = number % 10;
+    let mut _number = 1234;
+    let number_four = _number % 10;
+    _number /= 10;
+    let number_three = _number % 10;
+    _number /= 10;
+    let number_two = _number % 10;
+    _number /= 10;
+    let number_one = _number % 10;
 
-    let sum = number_four + number_three + number_two + number_one;
+    let _sum = number_four + number_three + number_two + number_one;
 
     //Task 4
-    //It's important tp specify numbers, because it will throw an error with unspecified ones
+    //It's important to specify numbers, because it will throw an error with unspecified ones
     let radius = 10.00;
     const PI: f64 = std::f64::consts::PI;
-    let l = (2.00 * PI) * radius;
-    let s = PI * (radius * radius);
-    let v = (4.00/3.00) * PI * (radius * radius * radius);
+    let _l = (2.00 * PI) * radius;
+    let _s = PI * (radius * radius);
+    let _v = (4.00/3.00) * PI * (radius * radius * radius);
 
     //Task 5
-    number = 123;
-    let three = number % 10;
-    number /= 10;
-    let two = number % 10;
-    number /= 10;
-    let one = number % 10;
-    number = (three * 100) + (two * 10) + one;
+    _number = 123;
+    let three = _number % 10;
+    _number /= 10;
+    let two = _number % 10;
+    _number /= 10;
+    let one = _number % 10;
+    _number = (three * 100) + (two * 10) + one;
 
     //It's necessary to see final results of each task before losing them.
     let pause_one = 1;
     let pause_two = 2;
-    let pause_result = pause_one + pause_two;
+    let _pause_result = pause_one + pause_two;
 }
--- task_1/tasks.md
+++ task_1/tasks.md
@@ -2,23 +2,27 @@
 
 ## Ukrainian language
 
-1. Дано дійсне число а. Користуючись тільки операціями множення, отримати значення: а4 – за дві операції, а6
-та а8 – за три операції, а10 – за чотири операції.
-2. Визначити квадрат відстані у просторі між двома точками M1 і M2 із заданими їх координатами (x1, y1, z1) і 
-(x2, y2, z2).
+1. Дано дійсне число а. Користуючись тільки операціями множення, отримати значення:
+   1. `а4` – за дві операції;
+   2. `а6` та `а8` – за три операції;
+   3. `а10` – за чотири операції.
+2. Визначити квадрат відстані у просторі між двома точками `M1` і `M2` із заданими їх координатами `(x1, y1, z1)` і 
+`(x2, y2, z2)`.
 3. Дано десяткове ціле 4-розрядне число. Визначити суму цифр заданого числа.
-4. За заданим радіусом r визначити довжину окружності l, площу кола S та об’єм кулі V.
+4. За заданим радіусом `r` визначити довжину окружності `l`, площу кола `S` та об’єм кулі `V`.
 5. Визначити число, яке отримане виписуванням у зворотному порядку цифр заданого тризначного числа в десятковій системі 
 числення.
 
 ## English language
 
-1. Given a real number a. Using only multiplication operations, get the following values: a4 – for two operations, a6
-and а8 – for three operations, а10 – for four operations.
-2. Determine the square of the distance in space between two points M1 and M2 with their given coordinates (x1, y1, z1)
-and (x2, y2, z2).
+1. Given a real number a. Using only multiplication operations, get the following values: 
+   1. `a4` – for two operations.
+   2. `a6` and `а8` – for three operations.
+   3. `а10` – for four operations.
+2. Determine the square of the distance in space between two points `M1` and `M2` with their given coordinates `(x1, y1, z1)`
+and `(x2, y2, z2)`.
 3. A 4-bit decimal integer is given. Determine the sum of the digits of the given number.
-4. Based on the given radius r, determine the length of the circle l, the area of the circle S, and the volume of the sphere V.
+4. Based on the given radius `r`, determine the length of the circle `l`, the area of the circle `S`, and the volume of the sphere `V`.
 5. Determine the number obtained by writing the digits of a given three-digit number in the reverse order in the decimal 
 system calculus.
 
